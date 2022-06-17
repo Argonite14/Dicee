67c3c0e2418bb6635ef330bf059ce61a5f1b43ac
Neil Johnson URK20CS1037 <81343012+Argonite14@users.noreply.github.com>
2022-06-17 22:57:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red.shade700,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red.shade900,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDice = Random().nextInt(6)+1;
+                    });
+
+                  },
+                  child: Image.asset('images/dice$leftDice.png')),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDice = Random().nextInt(6)+1;
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDice.png')),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
